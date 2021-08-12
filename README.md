5cb87a4007fa33475483dc0253a90a59723bfb17
Bret Cameron <bret@yulife.com>
2021-08-12 23:15:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Log File Reader
+
+This project contains a script to help extract information from a 3D Systems RTF log file.
+
+To use this project, you need [Node.js](https://nodejs.org/en/) v14+ installed.
+
+## Running the Script
+
+To run the script, navigate to this project in the terminal. Then run:
+
+```sh
+node . /path/to/your/log-file.rtf
+```
+
+To search for a particular string, you can add an additional argument inside quotation marks. For example:
+
+```sh
+node . /path/to/your/log-file.rtf "umbilical heater"
+```
+
+## Development
+
+Install [Yarn](https://yarnpkg.com/), then go to the terminal and run:
+
+```sh
+yarn
+```
+
+You can develop the project simply by following the instructions above
---
+++ index.js
@@ -0,0 +1,77 @@
+import { readFileSync } from "fs";
+
+if (process.argv.length < 3) {
+  throw new Error("A path to the log file must be provided.");
+}
+
+const filePath = process.argv[2];
+const searchQuery = process.argv?.[3];
+
+const lines = readFileSync(filePath, "utf8").split(`\n`);
+
+const obj = {};
+
+const regex = {
+  model: /(?<=\[ ba8\] Model:\s+).+(?=\\par)/,
+  serialNumber: /(?<=\[ ba8\] S\/N:\s+).+(?=\\par)/,
+  printerName: /(?<=\[ ba8\] Printer\sName:\s+).+(?=\\par)/,
+  version: /(?<=\[ ba8\] Version:\s+).+(?=\\par)/,
+  networkConfig: /(?<=\[ ba8\] Network\sConfig:\s+).+(?=\\par)/,
+  clock: /(?<=\[ ba8\] Clock:\s+).+(?=\\par)/,
+};
+
+for (const line of lines) {
+  if (regex.model.test(line)) {
+    obj.model = line.match(regex.model)[0].trim();
+  } else if (regex.serialNumber.test(line)) {
+    obj.serialNumber = line.match(regex.serialNumber)[0].trim();
+  } else if (regex.printerName.test(line)) {
+    obj.printerName = line.match(regex.printerName)[0].trim();
+  } else if (regex.version.test(line)) {
+    const fullString = line.match(regex.version)[0].trim();
+    obj.version = {
+      number: fullString.match(/(?<=\')[\d\.]+(?=\')/)[0],
+      time: fullString.match(/(?<=\'[\d\.]+',\s).*/)[0],
+    };
+  } else if (regex.networkConfig.test(line)) {
+    obj.networkConfig = line.match(regex.networkConfig)[0].trim();
+  } else if (regex.clock.test(line)) {
+    obj.clock = line.match(regex.clock)[0].trim().slice(0, 24);
+  }
+
+  if (/SetFailureInfo/i.test(line)) {
+    obj.failureInfo = obj.failureInfo || new Set();
+    obj.failureInfo.add(line.slice(50).trim().replace("\\par", ""));
+  }
+
+  if (searchQuery?.test(line)) {
+    obj.query = obj.query || new Set();
+    obj.query.add(line.slice(50).trim().replace("\\par", ""));
+  }
+}
+
+const { model, serialNumber, printerName, version, clock, failureInfo, query } =
+  obj;
+
+const { log, table } = console;
+const cyan = "\x1b[36m%s\x1b[0m";
+
+log(cyan, "MODEL INFO");
+
+table({
+  Model: model,
+  ["Serial Number"]: serialNumber,
+  ["Printer Name"]: printerName,
+  Version: version.number,
+  ["Time of Version"]: version.time,
+  ["Time of Log"]: clock,
+});
+
+log(cyan, "FAILURE INFO");
+
+table(failureInfo);
+
+if (query) {
+  log(cyan, "SEARCH QUERY");
+  table(query);
+}
